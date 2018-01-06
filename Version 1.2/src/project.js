bb215112443edf8eb132e193689f22b78c8ccefa
Luke12345678909 <31909239+Luke12345678909@users.noreply.github.com>
2018-01-05 17:13:16 -0800

    Create project.js

---
+++ Version 1.2/src/project.js
@@ -0,0 +1,14 @@
+function calculate(arg1,arg2){
+ if(arg2||arg2===0){
+   return (arg1===arg2);
+ }
+  else{
+    return arg1;    
+  }
+}
+function add(arg1,arg2){
+return arg1+arg2;
+}
+function subtract(arg1,arg2){
+return arg1-arg2;
+}
